c5b7d9fb24a48e00bd29473ed7d961c63d66954f
Kirill1709 <kirill.gatsuk13@gmail.com>
2021-11-10 21:05:44 +0300

    update admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Favourites, Ingredients, Recipe, Tag
+from .models import Favourites, Ingredients, Recipe, ShopingCart, Tag
 
 
 @admin.register(Recipe)
@@ -26,6 +26,13 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Favourites)
 class FavouritesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+    list_filter = ('user',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(ShopingCart)
+class ShopingCartAdmin(admin.ModelAdmin):
     list_display = ('user', 'recipe')
     list_filter = ('user',)
     empty_value_display = '-пусто-'
