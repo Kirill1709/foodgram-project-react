fda3e0317065d8e950cc9eca9a4d912b7c242ba0
Kirill1709 <kirill.gatsuk13@gmail.com>
2021-11-17 02:04:53 +0300

    fix secret key

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,7 +11,7 @@ env = environ.Env(
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'kjnkjnkjnkjnkjn')
 
 DEBUG = os.getenv('DEBUG', False)
 
